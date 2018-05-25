6944a27edece524af963fa7eb09a884365dc940c
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-05-25 13:10:25 +0300

    generate HeaderContainer component via react-redux.connect

--- src/containers/HeaderContainer.js
+++ src/containers/HeaderContainer.js
@@ -1,18 +1,6 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Header from '../components/Header';
-
-class HeaderContainer extends Component {
-  render() {
-    return <Header subheading={this.props.subheading} />;
-  }
-}
-
-HeaderContainer.propTypes = {
-  subheading: PropTypes.string.isRequired,
-};
 
 const extractHeadingFromPathname = pathname => {
   switch (true) {
@@ -31,4 +19,4 @@ const mapStateToProps = state => ({
   subheading: extractHeadingFromPathname(state.router.location.pathname),
 });
 
-export default connect(mapStateToProps)(HeaderContainer);
+export default connect(mapStateToProps)(Header);
