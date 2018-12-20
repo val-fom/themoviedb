5f45c258001e3f9984b9a47941b469756014337f
Valentyn Fomenko <fomenko.vn@gmail.com>
2018-12-20 20:46:01 +0200

    Updates

---
+++ precache-manifest.a4e37bec2172b6f96eff95be6f27f739.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c25c445e82ce586c70c1",
+    "url": "/themoviedb/static/js/runtime~main.c25c445e.js"
+  },
+  {
+    "revision": "6fec6640cebab7b9a808",
+    "url": "/themoviedb/static/js/main.6fec6640.chunk.js"
+  },
+  {
+    "revision": "6b05e54c2ab5d17302a1",
+    "url": "/themoviedb/static/js/1.6b05e54c.chunk.js"
+  },
+  {
+    "revision": "6fec6640cebab7b9a808",
+    "url": "/themoviedb/static/css/main.c77d2b22.chunk.css"
+  },
+  {
+    "revision": "9721d14f8ca3815e486ffdd119179afd",
+    "url": "/themoviedb/index.html"
+  }
+];
